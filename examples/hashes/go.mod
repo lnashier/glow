6676dd58c95472efc978cb9dc97d0d3471c6dc13
lnashier <17622088+lnashier@users.noreply.github.com>
2024-05-16 09:21:52 -0700

    +random hashes

---
+++ examples/hashes/go.mod
@@ -0,0 +1,12 @@
+module hashes
+
+go 1.22.3
+
+replace github.com/lnashier/glow => ../../../glow
+
+require github.com/lnashier/glow v0.0.0
+
+require (
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+)
---
+++ examples/hashes/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/lnashier/glow/flow"
+	"strings"
+	"time"
+)
+
+func main() {
+	found := false
+
+	err := flow.Sequential().
+		Read(func(ctx context.Context, emit func(any)) error {
+			for !found {
+				select {
+				case <-ctx.Done():
+					return nil
+				default:
+					randomBytes := make([]byte, 16)
+					_, err := rand.Read(randomBytes)
+					if err != nil {
+						return err
+					}
+					hasher := sha256.New()
+					_, err = hasher.Write(randomBytes)
+					if err != nil {
+						return err
+					}
+					emit(hex.EncodeToString(hasher.Sum(nil)))
+				}
+			}
+			return nil
+		}, flow.StepKey("generator"), flow.Concurrency(10), flow.Distributor()).
+		Filter(func(in any) bool {
+			return strings.HasPrefix(in.(string), "000000")
+		}, flow.StepKey("filter"), flow.Concurrency(10)).
+		Peek(func(in any) {
+			found = true
+			fmt.Println(in.(string))
+		}, flow.StepKey("print")).
+		Draw("bin/network.gv").
+		Run(context.Background()).
+		Uptime(func(d time.Duration) {
+			fmt.Println(d)
+		}).
+		Draw("bin/network-tally.gv").
+		Error()
+
+	if err != nil {
+		fmt.Println("err:", err)
+		return
+	}
+}
